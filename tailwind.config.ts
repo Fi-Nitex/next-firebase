79224d9029d11b6f49b33f43a9a9192aa33f7705
Grenish rai <107925840+Grenish@users.noreply.github.com>
2024-12-16 08:39:06 +0530

    Refactor tailwind.config.ts to update color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,30 +9,22 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: {
-          50: '#f0f9ff',
-          100: '#e0f2fe',
-          200: '#bae6fd',
-          300: '#7dd3fc',
-          400: '#38bdf8',
-          500: '#0ea5e9',
-          600: '#0284c7',
-          700: '#0369a1',
-          800: '#075985',
-          900: '#0c4a6e',
+        dark:{
+          50: "#1A1A1A",
+          100: "#04151f",
+          200: "#0A0903",
         },
-        secondary: {
-          50: '#fdf4ff',
-          100: '#fae8ff',
-          200: '#f5d0fe',
-          300: '#f0abfc',
-          400: '#e879f9',
-          500: '#d946ef',
-          600: '#c026d3',
-          700: '#a21caf',
-          800: '#86198f',
-          900: '#701a75',
+        primary:{
+          100: "#9FB8AD"
         },
+        text:{
+          50: "#E6E8E6",
+          100: "#CED0CE",
+        },
+        accent: {
+          50:"#FFEAAE",
+          100:"#CADF9E",
+        }
       },
       animation: {
         'fade-in': 'fadeIn 0.5s ease-out',
